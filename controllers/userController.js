31d0bbe5202a8c20c3aa3e920f18b99076ff6306
saway11 <rgarcia20.m@gmail.com>
2023-06-18 19:01:38 -0700

    function to create a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,3 +28,13 @@ const getUserById = async (req, res) => {
 };
 
 // create a user
+const createUser = async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+// update user by id
